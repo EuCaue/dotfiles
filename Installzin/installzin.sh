b301baabdf2b3333aaa943ccb450f8d7c7d139a1
EuCaue <caueadriano.castro@gmail.com>
2022-04-14 22:15:02 -0300

    Installzin

---
+++ Installzin/installzin.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+paru -S flatpak neovim google-chrome discord spotify-adblock steam telegram-desktop fish visual-studio-code-bin neofetch gnome-boxes ttf-fira-code ttf-fira-sans ttf-jetbrains-mono ttf-cascadia-code pacman-contrib pipewire-media-session pipewire-pulse pipewire-alsa gst-plugin-pipewire simplescreenrecorder solanum peek timeshift megasync lutris pavu drawing fragments
---
+++ Installzin/installzinFlatpak.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+flatpak install flathub org.gimp.GIMP -y && 
+flatpak installl flathub org.gnome.gitlab.somas.Apostrophe -y &&
+flatpak install flathub org.pitivi.Pitivi -y && 
+flatpak install flathub com.github.tchx84.Flatseal -y && 
+flatpak install flathub com.rtosta.zapzap -y &&
+flatpak install flathub io.github.hmlendea.geforcenow-electron -y&&
+flatpak install flathub org.onlyoffice.desktopeditors -y && 
+flatpak install flathub org.gabmus.giara -y && 
+flatpak install flathub com.rafaelmardojai.Blanket -y 
+
+
