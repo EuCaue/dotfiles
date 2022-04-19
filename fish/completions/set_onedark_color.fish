d702ceb7a3112514b0ebd1be40d46b74b891db10
EuCaue <caueadriano.castro@gmail.com>
2022-04-18 21:37:48 -0300

    Update fish config

---
+++ fish/completions/set_onedark_color.fish
@@ -0,0 +1,5 @@
+# set_onedark_color
+complete -c set_onedark_color -f -d 'define color for set_onedark'
+
+complete -c set_onedark_color -s h -l help -d 'show help message'
+complete -c set_onedark_color -r -f -a 'black brblack red brred green brgreen yellow bryellow blue brblue magenta brmagenta cyan brcyan white brwhite'
