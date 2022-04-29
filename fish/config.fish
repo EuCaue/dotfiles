bb165bcdb7d09725f4f20706a6b82a6a4c79e74a
EuCaue <caueadriano.castro@gmail.com>
2022-04-29 14:48:44 -0300

    New config

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,31 @@
-# Start X at login
-if status is-login
-    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-        exec startx -- -keeptty
-    end
-end
-
+# Init startship prompt
 starship init fish | source
-
+# Disable fish greeting
 set fish_greeting
  
+ # Alias for commands
+alias cfg='nvim ~/.config/fish/config.fish'
+alias update='paru && flatpak update'
+alias gte='gnome-text-editor'
+alias li='free -h && sudo limpante && free -h'
+alias f='free -h'
 
+# Theme for gnome-terminal
+alias gogh='bash -c "$(wget -qO- https://git.io/vQgMr)"'
+
+# Flatpak session
+alias fup='flatpak update'
+alias frm='flatpak uninstall --unused'
+alias ftam='flatpak --columns=name,size list'
+alias fli='sudo rm -rfv /var/tmp/flatpak-cache-*'
+
+# Exporting the default $EDITOR
+export EDITOR=/bin/nano
+export VISUAL=nano
+
+
+
+# *FETCH'S
 #freshfetch
 #ufetch
 #neofetch | lolcat
@@ -18,31 +34,11 @@ set fish_greeting
 #fastfetch | lolcat
 #afetch | lolcat
 #free -h
-alias cfg='nvim ~/.config/fish/config.fish'
-alias hi='echo "Hello World"'
-alias li='free -h && sudo limpante && free -h'
-alias gte='gnome-text-editor'
-alias update='paru && flatpak update'
-alias f='free -h'
-alias gogh='bash -c "$(wget -qO- https://git.io/vQgMr)"'
-alias ftam='flatpak --columns=name,size list'
-alias fli='sudo rm -rfv /var/tmp/flatpak-cache-*'
-alias frm='flatpak uninstall --unused'
-alias fup='flatpak update'
-#alias aura="sudo aura"
-export EDITOR=/bin/nano
-export VISUAL=nano
 
-#   set -l onedark_options '-b'
-    #if set -q VIM
-        # Using from vim/neovim.
-     #   set onedark_options "-256"
-    #else if string match -iq "eterm*" $TERM
-     #   # Using from emacs.
-    #    function fish_title; true; end
-   #     set onedark_options "-256"
-  #  end
-
- #   set_onedark $onedark_options
-#end
+# Start X at login
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
+end
 
