716fb04cd2e2a8fe3d1894e956106cf78b28f141
EuCaue <caueadriano.castro@gmail.com>
2022-10-27 08:45:38 -0300

    update intall script

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -10,6 +10,10 @@ local options = {
   mouse = "a",                             -- allow the mouse to be used in neovim
   pumheight = 10,                          -- pop up menu height
   showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
+  foldmethod = indent,
+  foldnestmax = 10,
+  nofoldenable,
+  foldlevel = 2,
   showtabline = 2,                         -- always show tabs
   smartcase = true,                        -- smart case
   smartindent = true,                      -- make indenting smarter again
--- scripts/gsettings.sh
+++ scripts/gsettings.sh
@@ -1,11 +1,11 @@
 #!/bin/fish
 
-gsettings set org.gnome.desktop.interface font-name "JetBrainsMono Nerd Font"
-gsettings set org.gnome.desktop.interface monospace-font-name "JetBrainsMono Nerd Font"
-gsettings set org.gnome.desktop.interface cursor-theme macOS-BigSur 
-gsettings set org.gnome.desktop.interface icon-theme Rose-Pine
-gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
-gsettings set org.gnome.desktop.interface cursor-size 16
-hyprctl setcursor macOS-BigSur 16
-export XCURSOR_THEME=macOS-BigSur
+gsettings set org.gnome.desktop.interface font-name "JetBrainsMono Nerd Font" &&
+gsettings set org.gnome.desktop.interface monospace-font-name "JetBrainsMono Nerd Font" &&
+gsettings set org.gnome.desktop.interface cursor-theme macOS-BigSur &&
+gsettings set org.gnome.desktop.interface icon-theme Rose-Pine &&
+gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark &&
+gsettings set org.gnome.desktop.interface cursor-size 16 &&
+hyprctl setcursor macOS-BigSur 16 &&
+export XCURSOR_THEME=macOS-BigSur &&
 export XCURSOR_SIZE=16
