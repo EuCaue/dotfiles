5ecc88fe85668cf6bad7a4e02e33c6062214a74b
EuCaue <caueadriano.castro@gmail.com>
2022-11-16 14:57:11 -0300

    waybar hyprland window

--- scripts/gsettings.sh
+++ scripts/gsettings.sh
@@ -3,10 +3,10 @@
 gsettings set org.gnome.desktop.interface font-name "JetBrainsMono Nerd Font" &&
 gsettings set org.gnome.desktop.interface document-font-name "JetBrainsMono Nerd Font" &&
 gsettings set org.gnome.desktop.interface monospace-font-name "JetBrainsMono Nerd Font" &&
-gsettings set org.gnome.desktop.interface cursor-theme Adwaita &&
+gsettings set org.gnome.desktop.interface cursor-theme Mocu-Black-Right &&
 gsettings set org.gnome.desktop.interface icon-theme Rose-Pine &&
 gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark &&
 gsettings set org.gnome.desktop.interface cursor-size 22 &&
-hyprctl setcursor Adwaita 22 &&
-export XCURSOR_THEME=Adwaita &&
+hyprctl setcursor Mocu-Black-Right 22 &&
+export XCURSOR_THEME=Mocu-Black-Right &&
 export XCURSOR_SIZE=22
