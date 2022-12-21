3b13eba979ef8b9fe4c018fb67d37620f182f9c6
EuCaue <caueadriano.castro@gmail.com>
2022-12-21 12:23:13 -0300

    update arch

--- installscripts/Arch/archinstall.sh
+++ installscripts/Arch/archinstall.sh
@@ -1,50 +1,50 @@
 #!/bin/sh 
  
 format_disk() {
-  loadkeys br-abnt2
-  lsblk 
+  loadkeys br-abnt2 &&
+  lsblk &&
   read -p "Select the disk: " -r DISK &&
   cfdisk "/dev/$DISK"
 }
 
 make_filesystem() {
-  lsblk
+  lsblk &&
   read -p "Root partition: " -r ROOT &&
   read -p "Boot Partition: " -r BOOT && 
   read -p "Filesystem: " -r FS &&
-  mkfs.$FS $ROOT
-  mkfs.fat -F 32 $BOOT
+  mkfs.$FS $ROOT &&
+  mkfs.fat -F 32 $BOOT &&
 }
 
 mount_fs() {
-  mount $ROOT /mnt
-  mount --mkdir $BOOT /mnt/boot
-}
+  mount $ROOT /mnt &&
+  mount --mkdir $BOOT /mnt/boot &&
+} 
 
 pacstrap_pkgs() {
   pacstrap -K /mnt base linux linux-firmware linux-headers linun-zen linux-zen-headers fish networkmanager nvim grub efibootmgr base-devel git curl
 }
 
 config_system() {
-  genfstab -U /mnt >> /mnt/etc/fstab
-  arch-chroot /mnt 
+  genfstab -U /mnt >> /mnt/etc/fstab &&
+  arch-chroot /mnt && 
    ln -sf /usr/share/zoneinfo/America/Bahia /etc/localtime &&
      hwclock --systohc
-   sed -i -e "/en_US.UTF-8 UTF-8/s/^#//" /etc/locale.gen 
-   locale-gen
-   touch /etc/locale.conf 
-   echo "LANG=en_US.UTF-8" > /etc/locale.conf
-   touch /etc/vconsole.conf 
-   echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
-   touch /etc/hostname 
-   echo "archBTW" > /etc/hostname
+   sed -i -e "/en_US.UTF-8 UTF-8/s/^#//" /etc/locale.gen && 
+   locale-gen &&
+   touch /etc/locale.conf && 
+   echo "LANG=en_US.UTF-8" > /etc/locale.conf &&
+   touch /etc/vconsole.conf && 
+   echo "KEYMAP=br-abnt2" > /etc/vconsole.conf &&
+   touch /etc/hostname && 
+   echo "archBTW" > /etc/hostname &&
    useradd -mG caue wheel 
-   mkinitcpio -P
+   mkinitcpio -P &&
    passwd caue
 }
 
 grub_install() {
-    grub-install --target=x86_64-efi --efi-directory=/boot --removable  --bootloader-id=archBT
+    grub-install --target=x86_64-efi --efi-directory=/boot --removable  --bootloader-id=archBT &&
     grub-mkconfig -o /boot/grub/grub.cfg
 }
 
