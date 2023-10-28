71b1b11a6f602e09cbd32bdf7c7b1fe618111567
EuCaue <caueadriano.castro@gmail.com>
2023-10-28 19:20:24 -0300

    better wezterm cfg && organize tmux config file

--- dotconfig/wezterm/wezterm.lua
+++ dotconfig/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 local config = {}
 
 if wezterm.config_builder then
@@ -23,7 +24,7 @@ local fonts = {
 	{ family = "Lilex Nerd Font", italic = italic },
 	{ family = "GoMono Nerd Font", italic = italic },
 	{ family = "Martian Mono", italic = italic },
-	{ family = "Maple Mono", italic = italic },
+	{ family = "Maple Mono", italic = italic, scale = 1.2 },
 }
 
 local emoji_font = { "Apple Color Emoji" }
@@ -34,13 +35,108 @@ config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 config.show_new_tab_button_in_tab_bar = true
 config.default_cursor_style = "BlinkingBar"
-config.use_fancy_tab_bar = true
+-- config.use_fancy_tab_bar = true
 config.window_close_confirmation = "NeverPrompt"
-config.enable_tab_bar = false
+-- config.enable_tab_bar = false
 config.scrollback_lines = 2500
-config.window_background_opacity = 0.4
+config.window_background_opacity = 0.8
+config.default_workspace = "home"
+config.window_decorations = "RESIZE"
 config.term = "wezterm"
 -- config.color_scheme = "Dark Pastel"
+
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+	-- Send C-a when pressing C-a twice
+	{ key = "a", mods = "LEADER", action = act.SendKey({ key = "a", mods = "CTRL" }) },
+	{ key = "c", mods = "LEADER", action = act.ActivateCopyMode },
+
+	-- Pane keybindings
+	{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	-- SHIFT is for when caps lock is on
+	{ key = "|", mods = "LEADER|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+	{ key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
+	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+	{ key = "s", mods = "LEADER", action = act.RotatePanes("Clockwise") },
+	-- We can make separate keybindings for resizing panes
+	-- But Wezterm offers custom "mode" in the name of "KeyTable"
+	{ key = "r", mods = "LEADER", action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
+
+	-- Tab keybindings
+	{ key = "n", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "[", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+	{ key = "]", mods = "LEADER", action = act.ActivateTabRelative(1) },
+	{ key = "t", mods = "LEADER", action = act.ShowTabNavigator },
+	-- Key table for moving tabs around
+	{ key = "m", mods = "LEADER", action = act.ActivateKeyTable({ name = "move_tab", one_shot = false }) },
+
+	-- Lastly, workspace
+	{ key = "w", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
+}
+
+config.key_tables = {
+	resize_pane = {
+		{ key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "Enter", action = "PopKeyTable" },
+	},
+	move_tab = {
+		{ key = "h", action = act.MoveTabRelative(-1) },
+		{ key = "j", action = act.MoveTabRelative(-1) },
+		{ key = "k", action = act.MoveTabRelative(1) },
+		{ key = "l", action = act.MoveTabRelative(1) },
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "Enter", action = "PopKeyTable" },
+	},
+}
+
+config.status_update_interval = 1000
+wezterm.on("update-right-status", function(window, pane)
+	-- Workspace name
+	local stat = window:active_workspace()
+	-- It's a little silly to have workspace name all the time
+	-- Utilize this to display LDR or current key table name
+	if window:active_key_table() then
+		stat = window:active_key_table()
+	end
+	if window:leader_is_active() then
+		stat = "LDR"
+	end
+
+	-- Current working directory
+	local basename = function(s)
+		-- Nothign a little regex can't fix
+		return string.gsub(s, "(.*[/\\])(.*)", "%2")
+	end
+	local cwd = basename(pane:get_current_working_dir())
+	-- Current command
+	local cmd = basename(pane:get_foreground_process_name())
+
+	-- Time
+	local time = wezterm.strftime("%H:%M")
+
+	-- Let's add color to one of the components
+	window:set_right_status(wezterm.format({
+		-- Wezterm has a built-in nerd fonts
+		{ Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
+		{ Text = " | " },
+		{ Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
+		{ Text = " | " },
+		{ Foreground = { Color = "FFB86C" } },
+		{ Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
+		"ResetAttributes",
+		{ Text = " | " },
+		{ Text = wezterm.nerdfonts.md_clock .. "  " .. time },
+		{ Text = " |" },
+	}))
+end)
 
 config.window_padding = {
 	left = 30,
