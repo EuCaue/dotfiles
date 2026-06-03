66e6ee9e24dad6022f3f46c2f2e4efaa74bbb062
EuCaue <souzacaue@proton.me>
2026-06-03 08:46:17 -0300

    feat: update

--- scripts/open-daily-note.sh
+++ scripts/open-daily-note.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env zsh
 
-URI="obsidian://open?vault=zk&file=journal/$(date +%F)"
+DATE=$(date +%F)
+FILE="journal/$DATE"
+VAULT="$HOME/Documents/zk"
+FILEPATH="$VAULT/$FILE.md"
+IS_NEW=false
 
-command -v obsidian >/dev/null 2>&1 \
-  && exec obsidian "$URI"
+if [[ ! -f "$FILEPATH" ]]; then
+  IS_NEW=true
+fi
 
-exec flatpak run md.obsidian.Obsidian "$URI"
+open_uri() {
+  if command -v obsidian >/dev/null 2>&1; then
+    obsidian "$1"
+  else
+    flatpak run md.obsidian.Obsidian "$1"
+  fi
+}
+
+if $IS_NEW; then
+  open_uri "obsidian://daily?vault=zk"
+else
+  open_uri "obsidian://open?vault=zk&file=$FILE"
+fi
